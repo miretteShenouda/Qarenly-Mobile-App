12742234b0ef36447acd25f17c2b49732beaf01f
Caty Ramy <catherineramy02@gmail.com>
2024-06-19 23:21:32 +0300

    adding design for the notifications sceen

--- lib/view/notification_screen/notifications_screen.dart
+++ lib/view/notification_screen/notifications_screen.dart
@@ -1,10 +1,10 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:qarenly/common/widgets/app_bar/app_bar.dart';
-import 'package:qarenly/repository/authentication%20repository/authentication_repo.dart';
-import 'package:qarenly/controller/notification_controller.dart';
 import 'package:get/get.dart';
+import 'package:qarenly/common/widgets/app_bar/app_bar.dart';
+import 'package:qarenly/controller/notification_controller.dart';
+import 'package:qarenly/repository/authentication%20repository/authentication_repo.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -14,9 +14,10 @@ class NotificationsPage extends StatefulWidget {
 }
 
 class _NotificationsPageState extends State<NotificationsPage> {
-  SearchController _searchController = Get.put(SearchController());
-  AuthenticationRepo _authenticationRepository = Get.put(AuthenticationRepo());
-  NotificationController _notificationController =
+  final SearchController _searchController = Get.put(SearchController());
+  final AuthenticationRepo _authenticationRepository =
+      Get.put(AuthenticationRepo());
+  final NotificationController _notificationController =
       Get.put(NotificationController());
 
   @override
@@ -30,40 +31,56 @@ class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            appBar: BuildAppBar(
-              searchController: _searchController,
-              authenticationRepo: _authenticationRepository,
-            ),
-            body: Obx(() {
-              if (_notificationController.isLoading.value) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (_notificationController.notifications.isEmpty) {
-                print(_notificationController.notifications.value);
-                return const Center(
-                  child: Text("No Notifications"),
-                );
-              } else {
-                return Expanded(
-                    child: NotificationListener(
-                  onNotification: (notification) {
-                    return true;
-                  },
-                  child: ListView.builder(
-                    itemCount:
-                        min(_notificationController.notifications.length, 10),
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                          title: Text(_notificationController
-                              .notifications[index]["title"]!),
-                          subtitle: Text(_notificationController
-                              .notifications[index]["body"]!));
-                    },
+      child: Scaffold(
+        appBar: BuildAppBar(
+          searchController: _searchController,
+          authenticationRepo: _authenticationRepository,
+        ),
+        body: Obx(() {
+          if (_notificationController.isLoading.value) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (_notificationController.notifications.isEmpty) {
+            return const Center(
+              child: Text("No Notifications"),
+            );
+          } else {
+            return ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: min(_notificationController.notifications.length, 10),
+              itemBuilder: (context, index) {
+                final notification =
+                    _notificationController.notifications[index];
+                return Card(
+                  margin: const EdgeInsets.symmetric(vertical: 6),
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ));
-              }
-            })));
+                  elevation: 2,
+                  child: ListTile(
+                    leading: Icon(Icons.notifications,
+                        color: Color.fromRGBO(0, 48, 73, 1)),
+                    title: Text(
+                      notification["title"]!,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(0, 48, 73, 1)),
+                    ),
+                    subtitle: Text(
+                      notification["body"]!,
+                      style: TextStyle(color: Color.fromRGBO(0, 48, 73, 0.6)),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 16),
+                  ),
+                );
+              },
+            );
+          }
+        }),
+      ),
+    );
   }
 }
